60157c13f4a47b4277a9aba736acade09f360804
Richard King <mail@richardskingdom.net>
2013-11-18 23:45:06 +0000

    Refactored tests to use a setUp() function

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -7,13 +7,19 @@ import tictactoe
 
 class TicTacToeTestCase(unittest.TestCase):
 
+	def setUp(self):
+		self.players = 2
+		self.cols = 3
+		self.rows = 3
+		self.game = tictactoe.Game(self.players, self.cols, self.rows)
+
 	def testGame(self):
 		self.game = tictactoe.Game()
 		self.assertIsInstance(self.game,tictactoe.Game)
-		self.assertEqual(self.game.players,2)
-		self.assertEqual(self.game.cols,3)
-		self.assertEqual(self.game.rows,3)
-		self.assertEqual(self.game.turns,9)
+		self.assertEqual(self.game.players,self.players)
+		self.assertEqual(self.game.cols,self.cols)
+		self.assertEqual(self.game.rows,self.rows)
+		self.assertEqual(self.game.turns,(self.cols*self.rows))
 
 	def testPlayer(self):
 		self.player = tictactoe.Player()
