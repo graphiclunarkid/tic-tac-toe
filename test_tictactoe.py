f0a2e39fe6012d695ceef3cb7f79456d53a13e7b
Richard King <mail@richardskingdom.net>
2013-11-19 22:41:23 +0000

    Brainless tests for illegal or repeat moves

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -29,13 +29,22 @@ class TicTacToeTestCase(unittest.TestCase):
 		self.assertIsInstance(self.p1,tictactoe.Player)
 		self.assertEqual(self.p1.squares,[])
 
-	def test_move(self):
+	def test_legal_move(self):
 		move1 = [0,0]
 		self.p1.move(move1)
 		self.assertEqual(self.game.turns,(self.cols*self.rows - 1))
 		self.assertEqual(len(self.p1.squares),1)
 		self.assertListEqual(self.p1.squares, [move1])
 		self.assertListEqual(self.game.board,[[0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]])
+
+	def test_illegal_move(self):
+		move1 = [-1,-1]
+		self.assertRaises(ValueError, self.p1.move(move1))
+
+	def test_repeat_move(self):
+		move1 = [0,0]
+		self.p1.move(move1)
+		self.assertRaises(ValueError, self.p1.move(move1))
 
 
 if __name__ == '__main__':
