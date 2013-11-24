71e9cb753305b03fcba6ee43991c402b2a23babb
Richard King <mail@richardskingdom.net>
2013-11-24 21:59:01 +0000

    Tidy up player titles

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ import tictactoe
 
 def getMove(player):
 	squares = { "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9 }
-	choice = input("Player " + str(player) + ", Pick a square (1-9): ")
+	choice = input("Player " + str(player + 1) + ", pick a square (1-9): ")
 	try:
 		return squares[choice]
 	except KeyError:
@@ -16,7 +16,7 @@ def getMove(player):
 if __name__ == '__main__':
 	g = tictactoe.Game()
 	while g.board.turns > 0 and g.winner is None:
-		currentPlayer = g.board.turns % g.players
+		currentPlayer = 1 - (g.board.turns % g.players)
 		move = getMove(currentPlayer)
 		try:
 			g.playerList[currentPlayer].move(move)
@@ -25,4 +25,4 @@ if __name__ == '__main__':
 	if g.winner is None:
 		print("It's a draw!")
 	else:
-		print("Player", currentPlayer, "wins!")
+		print("Player", currentPlayer + 1, "wins!")
