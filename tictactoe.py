f005d0ab7787209063ce90de73fee21d079b7f90
Richard King <mail@richardskingdom.net>
2013-11-24 22:39:38 +0000

    Document tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,18 @@
 
 class Board():
 
+	""" A class to represent a tic-tac-toe board, which comprises 9 squares
+
+	Attributes:
+
+		squares		List of squares that are available to be
+				picked.
+		startSquares	List of squares that were available when the
+				game started.
+		turns		The max number of turns remaining (number of
+				squares left to be picked).
+	"""
+
 	def __init__(self):
 		self.squares = [ x for x in range(1,10) ]
 		self.startSquares = list(self.squares)	# Copies the list rather than creating a second reference to it.
@@ -14,6 +26,21 @@ class Board():
 
 
 class Player():
+
+	""" A class to represent a player.
+
+	Attributes:
+
+		squares		The set of squares this player has picked.
+
+	Methods:
+
+		move()		Attempt to pick the specified square.
+				Throws a MoveError exception if the square
+				doesn't exist or has been taken already.  Sets
+				the player as the game winner if their set of
+				picked squares contains a winning line.
+	"""
 
 	def __init__(self, game, squares = None):
 		self.game = game
@@ -39,6 +66,21 @@ class Player():
 
 
 class Game():
+
+	""" A class to represent a game of tic-tac-toe.
+
+	Attributes:
+
+		winner		The player object that has won the game.
+		board		Object containing the playing surface
+		players		The number of players involved in this game.
+				(Two for real games, but some of the tests use
+				a single player)
+		playerList	List of player objects involved in this game.
+		winMoves	List of lists representing all possible winning
+				combinations of squares (rows, columns and
+				diagonals.)
+	"""
 
 	def __init__(self, players = 2):
 		self.winner = None
@@ -68,15 +110,21 @@ class Game():
 
 
 class Error(Exception):
-	"""Base class for exceptions in this module"""
+
+	"""
+	Base class for exceptions in this module.
+	"""
+	
 	pass
 
 
 class MoveError(Error):
-	"""Exception raised for invalid player moves
+
+	"""
+	Exception raised for invalid player moves.
 
 	Attributes:
-		msg -- explanation of the error
+		msg	Explanation of the error.
 	"""
 	
 	def __init__(self, msg):
@@ -84,6 +132,15 @@ class MoveError(Error):
 
 
 def getMove(player):
+
+	"""
+	A function to prompt players for their moves.
+	
+	If the player picks a move not in the "squares" list a KeyError
+	exception is thrown however we pass this so that the MoveError
+	exception will be thrown by the Player.move() method instead.
+	"""
+
 	squares = { "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9 }
 	choice = input("Player " + str(player + 1) + ", pick a square (1-9): ")
 	try:
@@ -93,6 +150,18 @@ def getMove(player):
 
 
 if __name__ == '__main__':
+
+	"""
+	This is the main game loop.
+
+	Process:
+		1. Create a new game with two players
+		2. First player is prompted to pick a square
+		3. Rotate between all players until one wins or all squares
+		have been picked.
+		4. Print a message depending on the outcome of the game
+	"""
+
 	g = Game()
 	while g.board.turns > 0 and g.winner is None:
 		currentPlayer = 1 - (g.board.turns % g.players)
@@ -105,3 +174,4 @@ if __name__ == '__main__':
 		print("It's a draw!")
 	else:
 		print("Player", currentPlayer + 1, "wins!")
+
