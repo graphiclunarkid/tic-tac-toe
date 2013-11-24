bfa2d379b723ba5037c84afd5fe435f4a1fabbf9
Richard King <mail@richardskingdom.net>
2013-11-24 21:19:18 +0000

    Fix whitespace, exception class-reference bug; use integers for squares

--- game.py
+++ game.py
@@ -5,24 +5,24 @@ import tictactoe
 
 
 def getMove(player):
-       squares = { "1":(0,0), "2":(0,1), "3":(0,2), "4":(1,0), "5":(1,1), "6":(1,2), "7":(2,0), "8":(2,1), "9":(2,2) }
-       choice = input("Player " + str(player) + ", Pick a square (1-9): ")
-       try:
-               return squares[choice]
-       except KeyError:
-               pass
+	squares = { "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9 }
+	choice = input("Player " + str(player) + ", Pick a square (1-9): ")
+	try:
+		return squares[choice]
+	except KeyError:
+		pass
 
 
 if __name__ == '__main__':
-       g = tictactoe.Game()
-       while g.board.turns > 0 and g.winner is None:
-               currentPlayer = g.board.turns % g.players
-               move = getMove(currentPlayer)
-               try:
-                       g.playerList[currentPlayer].move(move)
-               except MoveError as e:
-                       print(e.msg)
-       if g.winner is None:
-               print("It's a draw!")
-       else:
-               print(g.winner, "wins!")
+	g = tictactoe.Game()
+	while g.board.turns > 0 and g.winner is None:
+		currentPlayer = g.board.turns % g.players
+		move = getMove(currentPlayer)
+		try:
+			g.playerList[currentPlayer].move(move)
+		except tictactoe.MoveError as e:
+			print(e.msg)
+	if g.winner is None:
+		print("It's a draw!")
+	else:
+		print("Player", currentPlayer, "wins!")
