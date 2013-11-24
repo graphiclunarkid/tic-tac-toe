f8c4947eaba91a1fe8e64b78d50641a661d711e8
Richard King <mail@richardskingdom.net>
2013-11-24 14:21:43 +0000

    Split Game class into Board and Game classes - step 1 (rename existing class)

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -12,11 +12,11 @@ class TicTacToeTestCase(unittest.TestCase):
 		self.players = 2
 		self.cols = 3
 		self.rows = 3
-		self.game = tictactoe.Game(self.players, self.cols, self.rows)
-		self.p1 = tictactoe.Player(self.game)
+		self.board = tictactoe.Board(self.players, self.cols, self.rows)
+		self.p1 = tictactoe.Player(self.board)
 
 	def tearDown(self):
-		self.game = None
+		self.board = None
 		self.p1 = None
 
 
@@ -25,12 +25,12 @@ class Test_Structure(TicTacToeTestCase):
 	These tests aren't very useful any more but cost nothing to keep
 	"""
 
-	def test_game(self):
-		self.assertIsInstance(self.game,tictactoe.Game)
-		self.assertEqual(self.game.players,self.players)
-		self.assertEqual(self.game.cols,self.cols)
-		self.assertEqual(self.game.rows,self.rows)
-		self.assertEqual(self.game.turns,(self.cols*self.rows))
+	def test_board(self):
+		self.assertIsInstance(self.board,tictactoe.Board)
+		self.assertEqual(self.board.players,self.players)
+		self.assertEqual(self.board.cols,self.cols)
+		self.assertEqual(self.board.rows,self.rows)
+		self.assertEqual(self.board.turns,(self.cols*self.rows))
 
 	def test_player(self):
 		self.assertIsInstance(self.p1,tictactoe.Player)
@@ -42,7 +42,7 @@ class Test_Behaviour(TicTacToeTestCase):
 
 	def setUp(self):
 		super().setUp()
-		self.p2 = tictactoe.Player(self.game)
+		self.p2 = tictactoe.Player(self.board)
 
 	def tearDown(self):
 		super().tearDown()
@@ -51,10 +51,10 @@ class Test_Behaviour(TicTacToeTestCase):
 	def test_legal_move(self):
 		move1 = (0,0)
 		self.p1.move(move1)
-		self.assertEqual(self.game.turns,(self.cols*self.rows - 1))
+		self.assertEqual(self.board.turns,(self.cols*self.rows - 1))
 		self.assertEqual(len(self.p1.squares),1)
 		self.assertListEqual(self.p1.squares, [move1])
-		self.assertListEqual(self.game.board,[(0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)])
+		self.assertListEqual(self.board.squares,[(0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)])
 
 	def test_illegal_move(self):
 		move1 = (-1,-1)
@@ -81,13 +81,13 @@ class Test_Behaviour(TicTacToeTestCase):
 
 
 class Test_Gameplay(TicTacToeTestCase):
-	"""Tests that the game runs correctly"""
+	"""Tests that the board runs correctly"""
 
 	def test_win_condition(self):
-		moves = [square for square in self.game.board if square[0] == 0]
+		moves = [square for square in self.board.squares if square[0] == 0]
 		for move in moves:
 			self.p1.move(move)
-		self.assertIs(self.p1,self.game.winner)
+		self.assertIs(self.p1,self.board.winner)
 
 
 if __name__ == '__main__':
--- tictactoe.py
+++ tictactoe.py
@@ -1,44 +1,44 @@
 #!/usr/bin/python3
 
 
-class Game():
+class Board():
 
 	def __init__(self, players = 2, cols = 3, rows = 3):
 		self.players = players
 		self.cols = cols
 		self.rows = rows
-		self.board = [(x, y) for x in range(cols) for y in range(rows)]
-		self.startBoard = list(self.board)	# Copies the list rather than creating a second reference to it.
+		self.squares = [(x, y) for x in range(cols) for y in range(rows)]
+		self.startSquares = list(self.squares)	# Copies the list rather than creating a second reference to it.
 		self.winner = None
 		self.winMoves = frozenset(((0,0),(0,1),(0,2)))
 
 	def get_turns(self):
-		return len(self.board)
+		return len(self.squares)
 
 	turns = property(get_turns)
 
 
 class Player():
 
-	def __init__(self, game, squares = None):
-		self.game = game
+	def __init__(self, board, squares = None):
+		self.board = board
 		if squares is None:	# Google for "default parameter values in python"
 			squares = []
 		self.squares = squares
 
 	def move(self, square):
 		try:
-			self.squares.append(self.game.board.pop(self.game.board.index(square)))
+			self.squares.append(self.board.squares.pop(self.board.squares.index(square)))
 		except ValueError:
 			if square in self.squares:
 				raise MoveError("You have taken that square already!")
-			elif square in self.game.startBoard:
+			elif square in self.board.startSquares:
 				raise MoveError("Another player has taken that square already!")
 			else:
 				raise MoveError("That square isn't on the board!")
 		else:
-			if set(map(tuple, self.squares)) == self.game.winMoves:
-				self.game.winner = self
+			if set(map(tuple, self.squares)) == self.board.winMoves:
+				self.board.winner = self
 
 
 class Error(Exception):
